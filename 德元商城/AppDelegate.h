8df0126dc5ed08b9156c501a487b3f18caf35e93
胡隆海 <hulonghai@hulonghaideimac.local>
2018-08-13 14:54:06 +0800

    Initial Commit

---
+++ 德元商城/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  德元商城
+//
+//  Created by 胡隆海 on 2018/8/13.
+//  Copyright © 2018年 胡隆海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 德元商城/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  德元商城
+//
+//  Created by 胡隆海 on 2018/8/13.
+//  Copyright © 2018年 胡隆海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
