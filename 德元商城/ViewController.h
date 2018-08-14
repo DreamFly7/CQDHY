680307b208eaa0257e8c863c1fb649a889c8b65c
胡隆海 <hulonghai@hulonghaideimac.local>
2018-08-14 09:03:53 +0800

    生成项目

--- 德元商城/ViewController.h
+++ 德元商城/ViewController.h
@@ -3,7 +3,7 @@
 //  德元商城
 //
 //  Created by 胡隆海 on 2018/8/13.
-//  Copyright © 2018年 胡隆海. All rights reserved.
+//  Copyright © 2018年 重庆德弘缘电子商务有限公司. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- 德元商城/ViewController.m
+++ 德元商城/ViewController.m
@@ -3,7 +3,7 @@
 //  德元商城
 //
 //  Created by 胡隆海 on 2018/8/13.
-//  Copyright © 2018年 胡隆海. All rights reserved.
+//  Copyright © 2018年 重庆德弘缘电子商务有限公司. All rights reserved.
 //
 
 #import "ViewController.h"
@@ -16,14 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
